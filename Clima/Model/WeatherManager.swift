d24b3ddef7d652a41898880f5ca74b6252c1cfe6
jasonfangmagic <jasonfangmagic@gmail.com>
2024-03-19 19:21:00 -0400

    add url

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -15,8 +15,27 @@ struct WeatherManager {
         if let url = URL(string: urlString){
             
         //2. Create a URL session
+            let session = URLSession(configuration: .default)
             
+        //3. Give the session a task
+            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            
+        //4. Start the task
+            task.resume()
             
         }
+    }
+    
+    func handle(data: Data?, response: URLResponse?, error: Error?){
+        if error != nil {
+            print(error!)
+            return
+        }
+        
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString)
+        }
+        
     }
 }
